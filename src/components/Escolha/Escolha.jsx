fc24084b1272f2bae46b04103c7b72303c7d38c0
Waloxs <waloxgustavo0@gmail.com>
2024-06-26 13:46:33 -0300

    build

--- src/components/Escolha/Escolha.jsx
+++ src/components/Escolha/Escolha.jsx
@@ -35,7 +35,7 @@ const Escolha = () => {
 
   return (
     <div className='flex justify-center' style={{width: '100vw'}}>
-      <div className='container flex flex-col justify-center items-center' style={{ background: '#fff', height: '100vh', gap: '30px', marginTop: '10px', maxWidth: '65rem', padding: '40px'}}>
+      <div className='container flex flex-col justify-center items-center' style={{ background: '#fff', height: '100vh', gap: '30px', marginTop: '10px', maxWidth: '65rem', padding: '20px'}}>
         <div className="flex self-start" style={{ maxWidth: '65rem' }}>
           <Link to="/">
             <img src={Logo} alt="Logo" style={{ width: '6rem', height: '1.10rem' }} />
